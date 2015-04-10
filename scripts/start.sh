94553269a28783a1c52d8eb01cd5dc10499ebe5a
Frédéric Bouquet <bouquet.frederic@gmail.com>
2015-04-10 08:04:45 +0200

    startup OK + TODO list

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 MINECRAFT_PORT=25567
 BUNGEE_PORT=25577
-docker run -d -P newland/spigot -p 25565:$MINECRAFT_PORT /opt/server/start.sh
-docker run -d -P newland/bungee -p 25577:$BUNGEE_PORT /opt/bungee/start.sh
+docker rm bungee
+docker rm spigot
+docker run -d --name bungee -p $BUNGEE_PORT:25577 newland/bungee
+docker run -d --name spigot --link bungee:bungee -p $MINECRAFT_PORT:25565 newland/spigot
